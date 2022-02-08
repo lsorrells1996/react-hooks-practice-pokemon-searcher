d68573a5e9d94da1b1737c2dc0d4e47acd642d01
Lorenzo Sorrells <lsorrells1996@gmail.com>
2022-02-08 14:36:31 -0600

    commit tree d529279c4a1cbe74d5b3c4c90d0376f15590fe0e

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,10 +2,10 @@ import React from "react";
 import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
-function PokemonCollection() {
+function PokemonCollection( {pokemans} ) {
   return (
     <Card.Group itemsPerRow={6}>
-      <h1>Hello From Pokemon Collection</h1>
+      {pokemans.map(pokemon => <PokemonCard key={pokemon.id} pokemon={pokemon} sprites={pokemon.sprites} /> )}
     </Card.Group>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-function Search() {
+function Search({filterPokemans}) {
   return (
     <div className="ui search">
       <div className="ui icon input">
-        <input className="prompt" />
+        <input onChange={filterPokemans} className="prompt" />
         <i className="search icon" />
       </div>
     </div>
